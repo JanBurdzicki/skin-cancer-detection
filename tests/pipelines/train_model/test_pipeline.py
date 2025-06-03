73d3ce5bba080e705deb04c02747fd58316d17af
viGor027 <vigor.wm@yahoo.com>
2025-06-03 14:28:19 +0200

    Added github actions, fixed tests

--- tests/pipelines/train_model/test_pipeline.py
+++ tests/pipelines/train_model/test_pipeline.py
@@ -2,11 +2,25 @@ import pytest
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
-from lightning.pytorch.callbacks import Callback
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
-from src.skin_cancer_detection.pipelines.train_model.nodes import train_model, prepare_callbacks
-import os
+
+from kedro.config import OmegaConfigLoader
+from src.skin_cancer_detection.pipelines.train_model.nodes import prepare_callbacks, train_model
+
+@pytest.fixture
+def config_loader():
+    return OmegaConfigLoader(
+        conf_source="conf",
+        env="test",
+        config_patterns={
+            "parameters_train_model": ["parameters_train_model.yml"]
+        }
+    )
+
+@pytest.fixture
+def params(config_loader):
+    return config_loader["parameters_train_model"]
 
 
 @pytest.fixture
@@ -20,66 +34,31 @@ def dummy_model():
 
 @pytest.fixture
 def dummy_dataloaders():
-    X = torch.rand(64, 1, 28, 28).to(dtype=torch.float32)
-    y = torch.randint(2, (64, 1)).to(dtype=torch.float32)
+    X = torch.rand(10, 1, 28, 28).to(dtype=torch.float32)
+    y = torch.randint(0, 2, (10, 1)).to(dtype=torch.float32)
     dataset = TensorDataset(X, y)
-    loader = DataLoader(dataset, batch_size=8)
-    return loader, loader  # train_loader, val_loader
+    loader = DataLoader(dataset, batch_size=2)
+    return loader, loader
 
 
-@pytest.fixture
-def dummy_callbacks():
-    checkpoints_dir = os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))),
-        'data',
-        '06_models',
-        'checkpoints'
-    )
-
-    class DummyCallback(Callback):
-        pass
-
-    return [DummyCallback(), ModelCheckpoint(
-        monitor="val_loss",
-        mode="min",
-        filename="best-model",
-        save_top_k=0
-    )]
-
-
-@pytest.fixture
-def dummy_params():
-    return {
-        "learning_rate": 0.01,
-        "epochs": 1,
-        "accelerator": "auto"
-    }
-
-
-@pytest.fixture
-def dummy_prep_callbacks_params():
-    return {
-        'es_patience': 1,
-        'ckpt_save_top_k': 0
-    }
-
-
-def test_prepare_callbacks(dummy_prep_callbacks_params):
-    callbacks = prepare_callbacks(dummy_prep_callbacks_params)
+def test_prepare_callbacks(params):
+    callbacks = prepare_callbacks(params["callback_params"])
     assert len(callbacks) == 2
     assert isinstance(callbacks[0], EarlyStopping)
     assert isinstance(callbacks[1], ModelCheckpoint)
 
 
-def test_train_model_node(dummy_model, dummy_dataloaders, dummy_callbacks, dummy_params):
-    # jak chcemy żeby ten test wywoływał nodea prepare_callbacks, to będziemy do tej funkcji
-    # podać dummy_prep_callbacks_params, zamiast dummy callbacks, czy to będzie lepsze?
-    # jak powinno być
+def test_train_model_with_config(params, dummy_model, dummy_dataloaders):
+    callbacks = prepare_callbacks(params["callback_params"])
     train_loader, val_loader = dummy_dataloaders
-    trained_model = train_model(
+
+    ckpt_callback = train_model(
         model=dummy_model,
         train_dataloader=train_loader,
         val_dataloader=val_loader,
-        callbacks=dummy_callbacks,
-        params=dummy_params
+        callbacks=callbacks,
+        params=params["train_hparams"]
     )
+
+    assert ckpt_callback is not None
+    assert isinstance(ckpt_callback, ModelCheckpoint)
--- tests/test_run.py
+++ tests/test_run.py
@@ -7,14 +7,30 @@ from pathlib import Path
 
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
+import logging
+
 
 # The tests below are here for the demonstration purpose
 # and should be replaced with the ones testing the project
 # functionality
 
 class TestKedroRun:
-    def test_kedro_run(self):
+    # def test_kedro_run(self):
+    #     bootstrap_project(Path.cwd())
+    #
+    #     with KedroSession.create(project_path=Path.cwd()) as session:
+    #         assert session.run() is not None
+
+    def test_kedro_catalog(self):
         bootstrap_project(Path.cwd())
 
-        with KedroSession.create(project_path=Path.cwd()) as session:
-            assert session.run() is not None
+        with KedroSession.create(project_path=Path.cwd(), env="test") as session:
+            context = session.load_context()
+            catalog = context.catalog
+            params = context.params
+            assert params["train_hparams"]["epochs"] == 1
+            logging.info(context.catalog)
+
+# każdy step na gh ma główne working directory, musi być ustawiony na nazwę tego repo
+# pip install -r requirements.txt
+# na gh musi być pip install -e .
