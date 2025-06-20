2fc75f6c92e84167d7100377d891ab91a864815b
Jan Burdzicki <janburdzicki@gmail.com>
2025-06-20 23:56:56 +0200

    feat: update pipeline_registry.py

--- src/skin_cancer_detection/pipeline_registry.py
+++ src/skin_cancer_detection/pipeline_registry.py
@@ -3,6 +3,14 @@
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from skin_cancer_detection.pipelines import (
+    data_processing,
+    data_restructuring,
+    model_training,
+    model_evaluation,
+    csv_label_fixing,
+)
+
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,6 +18,21 @@ def register_pipelines() -> dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    data_processing_pipeline = data_processing.create_pipeline()
+    data_restructuring_pipeline = data_restructuring.create_pipeline()
+    model_training_pipeline = model_training.create_pipeline()
+    model_evaluation_pipeline = model_evaluation.create_pipeline()
+    csv_label_fixing_pipeline = csv_label_fixing.create_pipeline()
+
+    return {
+        "data_processing": data_processing_pipeline,
+        "data_restructuring": data_restructuring_pipeline,
+        "model_training": model_training_pipeline,
+        "model_evaluation": model_evaluation_pipeline,
+        "csv_label_fixing": csv_label_fixing_pipeline,
+        "__default__": data_processing_pipeline
+        + data_restructuring_pipeline
+        + model_training_pipeline
+        + model_evaluation_pipeline
+        + csv_label_fixing_pipeline,
+    }
