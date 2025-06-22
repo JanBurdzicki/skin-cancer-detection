aede883830060209396995c7a89109bab2af5580
Jan Burdzicki <janburdzicki@gmail.com>
2025-06-22 23:48:57 +0200

    feat: update pipeline_registry.py

--- src/skin_cancer_detection/pipeline_registry.py
+++ src/skin_cancer_detection/pipeline_registry.py
@@ -10,6 +10,8 @@ from skin_cancer_detection.pipelines import (
     data_cleanup,
     csv_joining,
     data_filtering,
+    image_padding_resizing,
+    image_labeling,
 )
 
 
@@ -25,6 +27,8 @@ def register_pipelines() -> dict[str, Pipeline]:
     data_cleanup_pipeline = data_cleanup.create_pipeline()
     csv_joining_pipeline = csv_joining.create_pipeline()
     data_filtering_pipeline = data_filtering.create_pipeline()
+    image_padding_resizing_pipeline = image_padding_resizing.create_pipeline()
+    image_labeling_pipeline = image_labeling.create_pipeline()
 
     return {
         "data_restructuring": data_restructuring_pipeline,
@@ -33,10 +37,14 @@ def register_pipelines() -> dict[str, Pipeline]:
         "data_cleanup": data_cleanup_pipeline,
         "csv_joining": csv_joining_pipeline,
         "data_filtering": data_filtering_pipeline,
+        "image_padding_resizing": image_padding_resizing_pipeline,
+        "image_labeling": image_labeling_pipeline,
         "__default__": data_restructuring_pipeline
         + csv_label_fixing_pipeline
         + csv_to_parquet_pipeline
         + data_cleanup_pipeline
         + csv_joining_pipeline
         + data_filtering_pipeline
+        + image_padding_resizing_pipeline
+        + image_labeling_pipeline
     }
