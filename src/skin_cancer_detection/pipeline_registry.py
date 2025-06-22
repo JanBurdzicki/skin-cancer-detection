7d9d4d99ba1b612904c5d3d0343e91e2f54f6b62
Jan Burdzicki <janburdzicki@gmail.com>
2025-06-22 19:42:23 +0200

    feat: update pipeline_registry.py

--- src/skin_cancer_detection/pipeline_registry.py
+++ src/skin_cancer_detection/pipeline_registry.py
@@ -7,8 +7,9 @@ from skin_cancer_detection.pipelines import (
     data_restructuring,
     csv_label_fixing,
     csv_to_parquet,
-    csv_joining,
     data_cleanup,
+    csv_joining,
+    data_filtering,
 )
 
 
@@ -21,8 +22,9 @@ def register_pipelines() -> dict[str, Pipeline]:
     data_restructuring_pipeline = data_restructuring.create_pipeline()
     csv_label_fixing_pipeline = csv_label_fixing.create_pipeline()
     csv_to_parquet_pipeline = csv_to_parquet.create_pipeline()
-    csv_joining_pipeline = csv_joining.create_pipeline()
     data_cleanup_pipeline = data_cleanup.create_pipeline()
+    csv_joining_pipeline = csv_joining.create_pipeline()
+    data_filtering_pipeline = data_filtering.create_pipeline()
 
     return {
         "data_restructuring": data_restructuring_pipeline,
@@ -30,9 +32,11 @@ def register_pipelines() -> dict[str, Pipeline]:
         "csv_to_parquet": csv_to_parquet_pipeline,
         "data_cleanup": data_cleanup_pipeline,
         "csv_joining": csv_joining_pipeline,
+        "data_filtering": data_filtering_pipeline,
         "__default__": data_restructuring_pipeline
         + csv_label_fixing_pipeline
         + csv_to_parquet_pipeline
         + data_cleanup_pipeline
         + csv_joining_pipeline
+        + data_filtering_pipeline
     }
